a6cf19e0a862d29dcd2f5b4234d5698598b271a0
xiuxiu <1831894878@qq.com>
2025-02-17 14:11:08 +0800

    duolingo hello

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,8 +1,17 @@
-fn main() {
-    println!("Hello, world!");
-    let mut x = 0;
-    while x < 5 {
-        println!("x is {}", x);
-        x += 1;
+fn greet_world () {
+    let chinese = "你好";
+    let english = "Hello";
+    let japanese = "こんにちは";
+    let french = "Bonjour";
+    let german = "Hallo";
+    let spanish = "Hola";
+    let italian = "Ciao";
+    let regions = [chinese, english, japanese, french, german, spanish, italian];
+    for region in regions { 
+        println!("{}", region);
     }
+}
+
+fn main () {
+    greet_world();
 }
